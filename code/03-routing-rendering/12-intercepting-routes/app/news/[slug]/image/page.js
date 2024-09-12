bfaeb4d447f1c6537212941aa7ea474def5fcf07
Nicola Eschbach <nicola.eschbach@hotmail.com>
2024-09-12 16:58:11 +0200

    section 4 done

--- code/03-routing-rendering/12-intercepting-routes/app/news/[slug]/image/page.js
+++ code/03-routing-rendering/12-intercepting-routes/app/news/[slug]/image/page.js
@@ -2,17 +2,24 @@ import { notFound } from 'next/navigation';
 
 import { DUMMY_NEWS } from '@/dummy-news';
 
-export default function ImagePage({ params }) {
+export default function InterceptedImagePage({ params }) {
   const newsItemSlug = params.slug;
-  const newsItem = DUMMY_NEWS.find((newsItem) => newsItem.slug === newsItemSlug);
+  const newsItem = DUMMY_NEWS.find(
+    (newsItem) => newsItem.slug === newsItemSlug
+  );
 
   if (!newsItem) {
     notFound();
   }
 
   return (
-    <div className="fullscreen-image">
-      <img src={`/images/news/${newsItem.image}`} alt={newsItem.title} />
-    </div>
+    <>
+      <div className="modal-backdrop" />
+      <dialog className="modal" open>
+        <div className="fullscreen-image">
+          <img src={`/images/news/${newsItem.image}`} alt={newsItem.title} />
+        </div>
+      </dialog>
+    </>
   );
 }
